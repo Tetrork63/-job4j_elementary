daf45f96b1090435ade9994031944ccf0dd8dcc7
UserDefault <1654466@gmail.com>
2022-10-14 17:10:55 +0300

    6. Checkstyle [#4123] Была одна ошибка с пробелом перд скобкой

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -6,7 +6,7 @@ public class Multiple {
         int result = 0;
         for (int i = 1; i <= 9; i++) {
             result = a * i;
-            System.out.println(a + "*" + i + "=" + result );
+            System.out.println(a + "*" + i + "=" + result);
 
         }
     }
