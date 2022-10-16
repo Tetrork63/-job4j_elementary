ef5e1e5e293463eb325577ee77f8bdc0b2199a4f
UserDefault <1654466@gmail.com>
2022-10-16 10:25:32 +0300

    5. Конвертер валюты [#41583 #351740] правка отступов

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -2,8 +2,8 @@ package ru.job4j.converter;
 
 public class Converter {
     public static float rubleToEuro(float value) {
-    float rsl = value / 70;
-    return rsl;
+        float rsl = value / 70;
+        return rsl;
 }
 
     public static float rubleToDollar(float value) {
