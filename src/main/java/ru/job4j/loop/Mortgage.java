2faad629df54ccbb35346837bead2e63c6c5d6bf
UserDefault <1654466@gmail.com>
2022-10-21 10:00:04 +0300

    5.7. Ипотека [#156315 #353127] правка

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -4,11 +4,8 @@ public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
         while (amount > 0) {
-            amount = (amount + (amount / 100 * percent)) - salary;
+            amount += amount * percent / 100 - salary;
             year++;
-            if (amount <= 0) {
-                break;
-            }
         }
         return year;
     }
